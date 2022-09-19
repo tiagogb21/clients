cf2b4f12716dbebe7e32a12ab8b93b7419ac6a1f
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-09-19 07:22:37 -0300

    feat(api/src/controller): :building_construction: add controller to enterprise

---
+++ api/src/controllers/enterprises.controller.js
@@ -0,0 +1,31 @@
+const EnterprisesService = require('../services/enterprises.service');
+
+const GenericError = require('../utils/GenericError');
+
+class EnterprisesController {
+  _service = new EnterprisesService();
+
+  getAllEnterprises = async (req, res) => {
+    try {
+      const enterprises = await this._service.getAllEnterprises();
+      return res.status(200).json(enterprises);
+    } catch (err) {
+      const error = new GenericError(err)
+      return res.status(error.getStatus()).json(error.getMessage());
+    }
+  }
+
+  getEnterpriseByName = async (req, res) => {
+    try {
+      console.log(1);
+      const { id, name } = req.params;
+      const enterprises = await this._service.getEnterpriseByName(id, name);
+      return res.status(200).json(enterprises);
+    } catch (err) {
+      const error = new GenericError(err)
+      return res.status(error.getStatus()).json(error.getMessage());
+    }
+  }
+}
+
+module.exports = EnterprisesController;
